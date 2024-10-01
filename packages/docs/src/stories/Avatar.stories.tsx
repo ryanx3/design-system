2554be774ebcc77ea33c3be06690c8380e18079e
Ryan <ryangabriel_cap@hotmail.com>
2024-10-01 14:39:55 +0100

    feat: Adicionado componente Avatar no Storybook com variante Primary e Fallback

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,21 @@
+import type { StoryObj, Meta } from "@storybook/react";
+import { Avatar, AvatarProps } from "@rxd-ui/react";
+
+export default {
+  title: "Data display/Avatar",
+  component: Avatar,
+  tags: ["autodocs"],
+  args: {
+    src: "https://github.com/ryanx3.png",
+    alt: "Ryan Martins",
+  },
+} as Meta<AvatarProps>;
+
+
+export const Primary: StoryObj<AvatarProps> = {};
+
+export const Fallback: StoryObj<AvatarProps> = {
+  args: { 
+    src: undefined
+  }
+};
