247aa8cb96f0742230be93288ffbaa379025ef99
Ryan <ryangabriel_cap@hotmail.com>
2024-10-04 15:30:53 +0100

    fix: Corrigindo subpath para o Storybook em produção

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -23,5 +23,11 @@ const config: StorybookConfig = {
     name: getAbsolutePath("@storybook/react-vite"),
     options: {},
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/design-system/';
+    }
+    return config;
+  }
 };
 export default config;
