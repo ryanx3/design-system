3f9a6ae265074b0b22dc531d30fdb1f404537067
Ryan <ryangabriel_cap@hotmail.com>
2024-10-07 23:18:08 +0100

    refactory: Refatorando estilização do componente Toast

--- packages/react/src/components/Toast/styles.ts
+++ packages/react/src/components/Toast/styles.ts
@@ -4,18 +4,27 @@ export const ToastContainer = styled("div", {
   borderRadius: "$sm",
   padding: "$3 $5",
   background: "$gray800",
-  width: "fit-content",
-  border: "1px solid $colors$gray600",
+  width: "22.5rem",
+  display: "flex",
+  justifyContent: "space-between",
 
+  border: "2px solid $colors$gray500",
 
+  fontFamily: "$default",
+  color:"$gray200",
+  fontSize: "$sm",
+  lineHeight:"$base",
+  
   svg: {
-    color: "$gray200"
-  }
+    color: "$gray200",
+    cursor: "pointer",
+  },
 });
 
 export const Title = styled("h3", {
-
-  svg: {
-    color: "$gray200"
-  }
+  color: "$white",
+  fontSize: "$lg",
+  fontWeight: "$bold",
+  lineHeight: "$base",
+  margin: 0
 });
